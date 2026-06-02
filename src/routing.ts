75e88b516a40bae290403f98a1c4506f367b2288
Allan Eggert <allaneggert1@gmail.com>
2026-06-02 08:22:00 -0300

    cleanup: remove duplicate root messages folder and unused routing.ts

--- src/routing.ts
+++
@@ -1,8 +0,0 @@
-import { defineRouting } from 'next-intl/routing';
-
-export const routing = defineRouting({
-  locales: ['pt-BR', 'es-PY'],
-  defaultLocale: 'pt-BR',
-});
-
-export type Locale = (typeof routing.locales)[number];
